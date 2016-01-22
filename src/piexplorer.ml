b55fb1e200593649777a5a09d108a4f6281352f1
Fredokun <fredokun@github.com>
2016-01-22 14:38:38 +0100

    header ok

--- src/piexplorer.ml
+++ src/piexplorer.ml
@@ -1,6 +1,15 @@
 
+open Version
+
+open Printf
+
+let abort (msg:string) (err_code:int) : unit =
+  printf "Now quitting\n  ==> %s\n\nBye bye !\n" msg ;
+  exit err_code
+
+
 let main () =
-  Printf.printf "PiExplorer ... TBA ...\n" ;;
+  Printf.printf "---------\nPiExplorer v.%d.%d-%s\n---------\n a state-space exploration & analysis tool for the Pi-calculus\n\n (C) %s-%s %s under the GPL 3 (cf. LICENSE)\n----------\n" _VERSION_MAJOR _VERSION_MINOR _VERSION_PATCH _START_YEAR _CURRENT_YEAR _AUTHOR  ;;
 
 
-main ()
+let _ = main ()
