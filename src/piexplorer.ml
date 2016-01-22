63a0f61c7ba7b402dda40c8d97be0ff59534a3fb
Fredokun <fredokun@github.com>
2016-01-22 14:59:36 +0100

    version

--- src/piexplorer.ml
+++ src/piexplorer.ml
@@ -7,6 +7,16 @@ let abort (msg:string) (err_code:int) : unit =
   printf "Now quitting\n  ==> %s\n\nBye bye !\n" msg ;
   exit err_code
 
+let parse_file (filename:string) : program =
+  try
+    (let in_file = open_in filename in
+     let lexbuf = Lexing.from_channel in_file in
+     let prog = Parser.program Lexer.token lexbuf in
+     close_in in_file ;
+     { prog with filename = filename } )
+  with Parse_Exception (msg, pos) ->
+    ( printf "Parse error: %s\n(%s)\n" msg (string_of_position pos) ) ;
+    abort("check your file", 1)
 
 let main () =
   Printf.printf "---------\nPiExplorer v.%d.%d-%s\n---------\n a state-space exploration & analysis tool for the Pi-calculus\n\n (C) %s-%s %s under the GPL 3 (cf. LICENSE)\n----------\n" _VERSION_MAJOR _VERSION_MINOR _VERSION_PATCH _START_YEAR _CURRENT_YEAR _AUTHOR  ;;
---
+++ src/version.ml
@@ -0,0 +1,12 @@
+
+let _VERSION_MAJOR = 0
+let _VERSION_MINOR = 0
+let _VERSION_PATCH = "dev1"
+
+let _START_YEAR = "2015"
+
+let _CURRENT_YEAR = "2016"
+
+let _AUTHOR = "Frederic Peschanski"
+
+
